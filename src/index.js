bcf040c1d5f728bfca9c961e9561c2380935105b
Mikalai Stadub <mikalaistadub@MacBook-Pro-Mikalai.local>
2020-10-15 21:49:56 +0300

    feat: resolve reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let rev = n.toString().split('').reverse();
+
+  for (let i =0; i <rev.length; i++){
+    if(rev[i] == '-'){
+        rev.splice(i, 1);
+    }
+  }
+  return rev.join('');
 }
